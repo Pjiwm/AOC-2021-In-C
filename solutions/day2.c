ae29e36578b9cc424001f3ff56faf7b02f50a19c
Pjiwm <Pim.Munne@gmail.com>
2021-12-02 06:21:27 +0100

    day2

--- solutions/day2.c
+++ solutions/day2.c
@@ -4,7 +4,44 @@
 #include "../helpers/file_reader.h"
 
 void day2() {
-    const size_t LENGTH = 1;
+    printf("day2:\n");
+    const size_t LENGTH = 1000;
     char* input[LENGTH];
-    // file_to_array("inputs/day1.txt", input);
+    file_to_array("inputs/day2.txt", input);
+    // part 1 variables
+    int forward = 0;
+    int depth = 0;
+    // part 2 variables
+    int aim = 0;
+    int new_depth = 0;
+    for (size_t i = 0; i < LENGTH; i++) {
+        int increment = input[i][strlen(input[i]) - 2] - '0';
+        if (input[i][0] == 'f') {
+            // part 1
+            forward += increment;
+            // part 2
+            new_depth += aim * increment;
+
+        }
+        else if (input[i][0] == 'd') {
+            // part 1
+            depth += increment;
+            // part 2
+            aim += increment;
+
+        }
+        else if (input[i][0] == 'u') {
+            // part 1
+            depth -= increment;
+            // part 2
+            aim -= increment;
+
+        }
+        else {
+            printf("something went wrong\n");
+        }
+    }
+    printf("%d\n", forward * depth);
+    printf("%d\n", forward * new_depth);
+
 }
